fb455ff6c7bd882e0af866d1bda2e79bd510b3d6
Mykola Stepanets <kolyastepanets@gmail.com>
2022-11-20 19:17:49 +0800

    make max 10 seconds delay when send request to google api

--- app/services/update_cell_background_color_in_expenses_page_async.rb
+++ app/services/update_cell_background_color_in_expenses_page_async.rb
@@ -2,11 +2,12 @@ class UpdateCellBackgroundColorInExpensesPageAsync
   include CallableService
 
   EVERY_DAY_EXPENSES_PAGE = 2079267030
+  MAKE_REQUEST_EVERY_SECONDS = 10
 
   def initialize(response_after_save_expenses, should_divide_expenses, index: nil)
     @response_after_save_expenses = response_after_save_expenses
     @should_divide_expenses = should_divide_expenses
-    @index = index || 0
+    @index = calculate_index(index)
   end
 
   def call
@@ -17,5 +18,15 @@ class UpdateCellBackgroundColorInExpensesPageAsync
 
   def line_number_saved_expenses
     @line_number_saved_expenses ||= @response_after_save_expenses.table_range.split(":")[-1].match(/\d.*/)[0].to_i
+  end
+
+  def calculate_index(index)
+    return 0 if index.nil?
+
+    if index > MAKE_REQUEST_EVERY_SECONDS
+      return MAKE_REQUEST_EVERY_SECONDS
+    end
+
+    index
   end
 end
