35bb069e5436371095996694d214d2c6d36e46a5
Mykola Stepanets <kolyastepanets@gmail.com>
2022-11-27 17:39:39 +0800

    remove useless config vcr

--- spec/support/vcr_and_mock.rb
+++ spec/support/vcr_and_mock.rb
@@ -5,10 +5,6 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = Rails.root.join('spec/vcr_cassets').to_s
 
-  c.preserve_exact_body_bytes do |http_message|
-    http_message.body.encoding.name == 'ASCII-8BIT' || !http_message.body.valid_encoding?
-  end
-
   c.default_cassette_options = {
     serialize_with: :json,
 
