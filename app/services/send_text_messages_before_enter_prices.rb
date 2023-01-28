a9cd122529fc75a56be11520de023f3743b808f4
Mykola Stepanets <kolyastepanets@gmail.com>
2023-01-28 11:39:38 +0800

    calculate difference between price with coins

--- app/services/send_text_messages_before_enter_prices.rb
+++ app/services/send_text_messages_before_enter_prices.rb
@@ -9,11 +9,11 @@ class SendTextMessagesBeforeEnterPrices
   def call
     if @is_usd
       total_sum_of_money = ReceiveUsdFopFromGoogleSheet.call
-      total_sum_of_money_before_save = total_sum_of_money.split("usd fop in google sheet: $")[-1].gsub(/[[:space:]]+/, "").to_f
+      total_sum_of_money_before_save = total_sum_of_money.split("usd fop in google sheet: $")[-1].gsub(/[[:space:]]+/, "").gsub(",", ".").to_f
     end
     if @is_uah
       total_sum_of_money = ReceiveCurrentBalanceInMonobankFromGoogleSheet.call
-      total_sum_of_money_before_save = total_sum_of_money.split("uah in google sheet: ")[-1].split("грн")[0].gsub(/[[:space:]]+/, "").to_f
+      total_sum_of_money_before_save = total_sum_of_money.split("uah in google sheet: ")[-1].split("грн")[0].gsub(/[[:space:]]+/, "").gsub(",", ".").to_f
     end
 
     Telegram.bot.send_message(chat_id: ENV['MY_TELEGRAM_ID'], text: "Общая сумма перед заполнением: \n#{total_sum_of_money}")
--- app/services/text_messages_after_enter_prices.rb
+++ app/services/text_messages_after_enter_prices.rb
@@ -10,11 +10,11 @@ class TextMessagesAfterEnterPrices
   def call
     if @is_usd
       total_sum_of_money = ReceiveUsdFopFromGoogleSheet.call
-      total_sum_of_money_after_save = total_sum_of_money.split("usd fop in google sheet: $")[-1].gsub(/[[:space:]]+/, "").to_f
+      total_sum_of_money_after_save = total_sum_of_money.split("usd fop in google sheet: $")[-1].gsub(/[[:space:]]+/, "").gsub(",", ".").to_f
     end
     if @is_uah
       total_sum_of_money = ReceiveCurrentBalanceInMonobankFromGoogleSheet.call
-      total_sum_of_money_after_save = total_sum_of_money.split("uah in google sheet: ")[-1].split("грн")[0].gsub(/[[:space:]]+/, "").to_f
+      total_sum_of_money_after_save = total_sum_of_money.split("uah in google sheet: ")[-1].split("грн")[0].gsub(/[[:space:]]+/, "").gsub(",", ".").to_f
     end
 
     {
