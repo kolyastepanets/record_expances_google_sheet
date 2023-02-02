83f4b5e4b963a8c9687bf5b45169537b95d4f419
Mykola Stepanets <kolyastepanets@gmail.com>
2023-02-02 16:00:47 +0800

    clear messages from redis in any case

--- app/services/delete_messages.rb
+++ app/services/delete_messages.rb
@@ -9,7 +9,7 @@ class DeleteMessages
     @message_ids.each do |message_id|
       Telegram.bot.delete_message(chat_id: ENV['MY_TELEGRAM_ID'], message_id: message_id)
     end
-
+  ensure
     key = "messages_at_#{Date.today.to_s}"
     Redis.new.del(key)
   end
