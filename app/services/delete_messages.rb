0369bc5493aa9bb8b8a69c2372cdb0e897dcb1d6
Mykola Stepanets <kolyastepanets@gmail.com>
2023-02-07 21:55:13 +0800

    try to delete all messages, do not fail

--- app/services/delete_messages.rb
+++ app/services/delete_messages.rb
@@ -10,7 +10,19 @@ class DeleteMessages
     Redis.new.del(key)
 
     @message_ids.each do |message_id|
-      Telegram.bot.delete_message(chat_id: ENV['MY_TELEGRAM_ID'], message_id: message_id)
+      delete_message(message_id)
+    end
+
+    nil
+  end
+
+  private
+
+  def delete_message(message_id)
+    Telegram.bot.delete_message(chat_id: ENV['MY_TELEGRAM_ID'], message_id: message_id)
+  rescue Telegram::Bot::Error => e
+    if e.message != "Bad Request: Bad Request: message to delete not found"
+      raise e
     end
   end
 end
