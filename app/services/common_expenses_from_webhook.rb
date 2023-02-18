4c6a11c8043a18d8b18ac8ea0830b66adad607b1
Mykola Stepanets <kolyastepanets@gmail.com>
2023-02-18 13:41:11 +0800

    setup auto payment for Mark 1st of month

--- app/services/common_expenses_from_webhook.rb
+++ app/services/common_expenses_from_webhook.rb
@@ -26,8 +26,11 @@ class CommonExpensesFromWebhook
     when youtube
       @params[:category_name] = 'Для дома'
       @params[:sub_category_name] = 'youtube'
-    when -> (description) { description == "Лиля ❤️" && Date.today.day == 1 }
+    when -> (description) { description == "Лиля ❤️" && Date.today.day == 1 && (@params[:price_in_uah].present? ? (@params[:price_in_uah] > 10000) : false) }
       @params[:category_name] = 'Лиля'
+      @params[:sub_category_name] = nil
+    when -> (description) { (description == "Лиля ❤️" || description == "Лілія С.") && Date.today.day == 1 && (@params[:price_in_uah].present? ? (@params[:price_in_uah] < 10000) : false) }
+      @params[:category_name] = 'Марк'
       @params[:sub_category_name] = nil
     when google_disk
       @params[:category_name] = 'Для дома'
