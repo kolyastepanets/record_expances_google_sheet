0ebeff5851f7dd1656709b188f6700ca3ddcb75b
Mykola Stepanets <kolyastepanets@gmail.com>
2023-04-19 10:16:52 +0800

    fix in find message id

--- app/services/find_message_id.rb
+++ app/services/find_message_id.rb
@@ -5,12 +5,13 @@ class FindMessageId
     @transaction_id = transaction_id
     @price = price
     @message_id_from_response = message_id_from_response
+    @redis = Redis.new
   end
 
   def call
     return @message_id_from_response if @transaction_id.blank?
 
-    params = JSON.parse(redis.get(@transaction_id))
+    params = JSON.parse(@redis.get(@transaction_id))
     return params["message_ids"].last if !params.is_a?(Array)
 
     last_price_to_message = params.select { |pri| pri["price"] == @price.to_f }[-1]
