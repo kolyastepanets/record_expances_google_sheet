ac79d79472cacda286c2802d9a18416cf6fa0fd1
Mykola Stepanets <kolyastepanets@gmail.com>
2023-11-18 20:56:39 +0800

    update script

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,5 +9,5 @@ every "0 2 * * 2,5" do
 end
 
 every 2.minutes do
-  command "bash -c 'response=$(curl --write-out '%{http_code}' --silent --output /dev/null https://google-sheet-expenses-tg-bot.pp.ua/health_checks); if [ $response -eq 200 ]; then cd /home/root/apps/record_expenses_google_sheet/current && ~/.rvm/bin/rvm default do bundle exec pumactl -S /home/root/apps/record_expenses_google_sheet/shared/tmp/pids/puma.state -F /home/root/apps/record_expenses_google_sheet/shared/puma.rb restart; fi'"
+  command "bash -c 'response=$(curl --write-out '%{http_code}' --silent --output /dev/null https://google-sheet-expenses-tg-bot.pp.ua/health_checks); if [ $response -ne 200 ]; then mkdir -p /home/root/apps/record_expenses_google_sheet/shared/tmp/sockets && mkdir -p /home/root/apps/record_expenses_google_sheet/shared/tmp/pids && cd /home/root/apps/record_expenses_google_sheet/current && ~/.rvm/bin/rvm default do bundle exec puma -C /home/root/apps/record_expenses_google_sheet/shared/puma.rb --daemon; fi'"
 end
