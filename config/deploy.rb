6654da361f81f7ce35faccbaf2bc80d5949981cf
Mykola Stepanets <kolyastepanets@gmail.com>
2024-01-12 20:33:43 +0800

    update capistrano to use ruby

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,8 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
+set :rvm_ruby_version, '2.7.0'
+
 # skip assets:precompile
 set :assets_roles, []
 
