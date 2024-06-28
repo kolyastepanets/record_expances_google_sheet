ed74fdf1b26388447ed3ab1f34a5a4ebdd56fbf4
Mykola Stepanets <kolyastepanets@gmail.com>
2024-06-28 14:11:24 +0100

    delete redis key after usage

--- app/services/send_message_to_bot_to_ask_to_enter_expences_from_monzo_schedule.rb
+++ app/services/send_message_to_bot_to_ask_to_enter_expences_from_monzo_schedule.rb
@@ -8,6 +8,7 @@ class SendMessageToBotToAskToEnterExpencesFromMonzoSchedule
   def call
     @redis.scan_each(match: 'monzo_transaction_id_*') do |key|
       params = JSON.parse(@redis.get(key)).deep_symbolize_keys
+      @redis.del(key)
       SendMessageToBotToAskToEnterExpencesFromMonzo.call(params)
     end
   end
